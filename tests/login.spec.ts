b3c11ae3313213d33cd9ae9cc0c4f438dcf8241d
sokhin-viktor <normalnok@gmail.com>
2025-08-09 15:42:28 +0300

    commit with some fix for the tenth unit

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -5,7 +5,7 @@ test('Successful login and account page verification', async ({ page }) => {
     await page.getByTestId('email').fill('customer@practicesoftwaretesting.com');
     await page.getByTestId('password').fill('welcome01');
     await page.getByRole('button', {name: 'Login'}).click();
-    await expect(page).toHaveURL('https://practicesoftwaretesting.com/account');
+    await expect(page).toHaveURL(/\/account$/);
     await expect(page.getByTestId('page-title')).toHaveText('My account');
     await expect( page.getByTestId('nav-menu')).toHaveText('Jane Doe');
 });
